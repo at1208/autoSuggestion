634a1a707ede48c9f8a9f63847c1dcdee5d660a5
at1208 <amantiwari12081999@gmail.com>
2019-09-23 13:47:31 +0530

    initial setup

---
+++ src/components/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Heading from './heading'
+
+const App = () => {
+  return <div>
+     <Heading />
+        </div>
+}
+export default App;
---
+++ src/components/heading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Heading = () => {
+  return <div className='container-fluid'>
+  <h1 className='text-center'>Search Engine</h1>
+  <hr />
+  </div>
+}
+export default Heading;
